e2c480e8ed508c9821b62faf3802002dad45ca7c
Ertuğrul Sencer <teknoertugrul@gmail.com>
2021-03-28 00:48:38 +0300

    loader duzenlendi, 404 bootstrap eklendi

--- static/404.php
+++ static/404.php
@@ -5,10 +5,12 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Aradığınız Sayfa Bulunamadı</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"/>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
   <link rel="stylesheet" href="<?=Helper::asset('css/style.min.css')?>"/>
 </head>
 <body id="Page404">
+  <?php include_once 'static/loader.php'?>
   <div class="container">
     <header>IMKB Message</header>
     <h1>Aradığınız Sayfa Bulunamadı</h1>
@@ -17,5 +19,6 @@
       <i class="fas fa-arrow-right"></i>
     </a>
   </div>
+  <script src="<?=Helper::asset('js/main.js')?>"></script>
 </body>
 </html>
