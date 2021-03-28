ffec28df212cdf2fe75e4caea26119c48fed855e
Ertuğrul Sencer <teknoertugrul@gmail.com>
2021-03-28 22:49:59 +0300

    Login, register boşluk kontrolu yapıldı

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -4,13 +4,22 @@ $settings = [
   'site_title' => $db->getSettings('site_title'),
 ];
 if (isset($_POST['login'])) {
-  if ($db->checkUser(Helper::post('user_name'), Helper::post('user_pass'))) {
-    Helper::setResponseAlert('login', '<div class="alert py-2 alert-success py-2 my-3">Başarı ile giriş yaptınız.</div>');
-    $_SESSION['user'] = $db->getUserId(Helper::post('user_name'));
-    header('Location: ' . Helper::url('profile'));
-  } else {
-    Helper::setResponseAlert('login', '<div class="alert py-2 alert-danger py-2 my-3">Kullanıcı adınız veya şifreniz hatalı, lütfen kontrol edip tekrar deneyiniz.</div>');
+  if (
+    empty(Helper::post('user_name')) ||
+    empty(Helper::post('user_pass'))
+  ) {
+    Helper::setResponseAlert('login', '<div class="alert alert-warning my-3">Lütfen tüm alanları doldurunuz !</div>');
     header('Location: ' . Helper::url('login'));
+    exit;
+  } else {
+    if ($db->checkUser(Helper::post('user_name'), Helper::post('user_pass'))) {
+      Helper::setResponseAlert('login', '<div class="alert alert-success my-3">Başarı ile giriş yaptınız.</div>');
+      $_SESSION['user'] = $db->getUserId(Helper::post('user_name'));
+      header('Location: ' . Helper::url('profile'));
+    } else {
+      Helper::setResponseAlert('login', '<div class="alert alert-danger my-3">Kullanıcı adınız veya şifreniz hatalı, lütfen kontrol edip tekrar deneyiniz.</div>');
+      header('Location: ' . Helper::url('login'));
+    }
+    exit;
   }
-  exit;
 }
--- app/controllers/register.php
+++ app/controllers/register.php
@@ -6,21 +6,33 @@ $settings = [
   'site_title' => $db->getSettings('site_title'),
 ];
 if (isset($_POST['register'])) {
-  $response = $db->registerUser([
-    Helper::post('user_name'),
-    Helper::post('frist_name'),
-    Helper::post('last_name'),
-    Helper::post('e_mail'),
-    Helper::post('user_pass'),
-  ]);
-  if ($response) {
-    $html = '<div class="alert alert-success">Başarı ile kayıt oldunuz, giriş yapabilirsiniz.</div>';
-    Helper::setResponseAlert('register', $html);
-    header('Location: ' . Helper::url('login'));
-    exit;
-  } else {
-    Helper::setResponseAlert('register', $response);
+  if (
+    empty(trim(Helper::post('user_name'))) ||
+    empty(trim(Helper::post('frist_name'))) ||
+    empty(trim(Helper::post('last_name'))) ||
+    empty(trim(Helper::post('e_mail'))) ||
+    empty(trim(Helper::post('user_pass')))
+  ) {
+    Helper::setResponseAlert('register', '<div class="alert alert-warning">Lütfen tüm alanları doldurunuz !</div>');
     header('Location: ' . Helper::url('register'));
     exit;
+  } else {
+    $response = $db->registerUser([
+      Helper::post('user_name'),
+      Helper::post('frist_name'),
+      Helper::post('last_name'),
+      Helper::post('e_mail'),
+      Helper::post('user_pass'),
+    ]);
+    if ($response) {
+      $html = '<div class="alert alert-success">Başarı ile kayıt oldunuz, giriş yapabilirsiniz.</div>';
+      Helper::setResponseAlert('register', $html);
+      header('Location: ' . Helper::url('login'));
+      exit;
+    } else {
+      Helper::setResponseAlert('register', $response);
+      header('Location: ' . Helper::url('register'));
+      exit;
+    }
   }
 }
