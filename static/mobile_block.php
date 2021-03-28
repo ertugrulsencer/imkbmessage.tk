26bedbc6e135d795ba940f589e67ec0637a1a51e
Ertuğrul Sencer <teknoertugrul@gmail.com>
2021-03-28 23:07:40 +0300

    Mobil girisler engellendi

---
+++ static/mobile_block.php
@@ -0,0 +1,5 @@
+<div class="mobile-block">
+  <div class="logo px-4 mt-0"><?=$settings['site_title']?></div>
+  <strong class="mt-auto px-4">Mobil Giriş Henüz Hazır Değil</strong>
+  <i class="fas fa-4x fa-times mb-auto mt-5"></i>
+</div>
