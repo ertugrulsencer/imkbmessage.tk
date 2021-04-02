7aa22a292c29b037a92fa8fdd10a99314e6d07e9
Ertuğrul Sencer <teknoertugrul@gmail.com>
2021-04-03 02:28:22 +0300

    alert'lere ikon eklendi (js)

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,23 @@ const els = {
 addEventListener("load", () => {
   setTimeout(() => els.loader.classList.add("hidden"), 1000);
 });
+/* Warning Icon */
+document.querySelectorAll(".alert-warning").forEach((alert) => {
+  const content = alert.textContent;
+  alert.innerHTML = '<i class="fas fa-exclamation-circle me-2"></i>' + content;
+});
+/* Danger Icon */
+document.querySelectorAll(".alert-danger").forEach((alert) => {
+  const content = alert.textContent;
+  alert.innerHTML = '<i class="fas fa-times-circle me-2"></i>' + content;
+});
+/* Success Icon */
+document.querySelectorAll(".alert-success").forEach((alert) => {
+  const content = alert.textContent;
+  alert.innerHTML = '<i class="fas fa-check-circle me-2"></i>' + content;
+});
+/* Info Icon */
+document.querySelectorAll(".alert-info").forEach((alert) => {
+  const content = alert.textContent;
+  alert.innerHTML = '<i class="fas fa-info-circle me-2"></i>' + content;
+});
